758d5c1fee32adbe8cb8dad532dc7fbaf804035c
Víctor Oliva <olivarra1@gmail.com>
2023-12-11 00:42:36 +0100

    Solution to part 2 day 5

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,10 +1,9 @@
-use itertools::Itertools;
-
 use super::Solver;
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::Range;
 use std::str::FromStr;
-use std::thread::current;
 
 pub struct Problem;
 
@@ -40,21 +39,91 @@ impl Mapping {
             None
         }
     }
+
+    fn map_range(self: &Self, range: &Range<usize>) -> (Option<Range<usize>>, Vec<Range<usize>>) {
+        let self_end = self.source + self.range;
+        let start = range.start.max(self.source);
+        let end = range.end.min(self_end);
+
+        if end > start {
+            let mut unmapped = Vec::new();
+            if start > range.start {
+                unmapped.push(Range {
+                    start: range.start,
+                    end: start,
+                })
+            }
+            if end < range.end {
+                unmapped.push(Range {
+                    start: end,
+                    end: range.end,
+                })
+            }
+            (
+                Some(Range {
+                    start: self.destination + start - self.source,
+                    end: self.destination + end - self.source,
+                }),
+                unmapped,
+            )
+        } else {
+            (None, vec![range.to_owned()])
+        }
+    }
+}
+
+struct Mappings {
+    mappings: Vec<Mapping>,
+}
+impl Mappings {
+    fn map_seed(self: &Self, seed: usize) -> usize {
+        self.mappings
+            .iter()
+            .find_map(|mapping| mapping.map(seed))
+            .unwrap_or(seed)
+    }
+
+    fn map_range(self: &Self, range: &Range<usize>) -> Vec<Range<usize>> {
+        let mut unmapped_ranges = vec![range.to_owned()];
+        let mut mapped_ranges = Vec::new();
+
+        for mapping in &self.mappings {
+            let mut new_unmapped_ranges = Vec::new();
+            for range in unmapped_ranges {
+                let (mapped, unmapped) = mapping.map_range(&range);
+                new_unmapped_ranges.extend(unmapped);
+                if let Some(mapped) = mapped {
+                    mapped_ranges.push(mapped);
+                }
+            }
+            unmapped_ranges = new_unmapped_ranges;
+        }
+
+        mapped_ranges.append(&mut unmapped_ranges);
+
+        merge_ranges(&mapped_ranges)
+    }
 }
 
 pub struct Almanac {
     seeds: Vec<usize>,
-    mappings: Vec<Vec<Mapping>>,
+    seed_ranges: Vec<Range<usize>>,
+    mappings: Vec<Mappings>,
 }
 
 impl Almanac {
     fn get_seed_location(self: &Self, seed: usize) -> usize {
-        self.mappings.iter().fold(seed, |acc, mappings| {
-            mappings
-                .iter()
-                .find_map(|mapping| mapping.map(acc))
-                .unwrap_or(acc)
-        })
+        self.mappings
+            .iter()
+            .fold(seed, |acc, mappings| mappings.map_seed(acc))
+    }
+
+    fn get_seed_range_location(self: &Self, range: &Range<usize>) -> Vec<Range<usize>> {
+        self.mappings
+            .iter()
+            .fold(vec![range.to_owned()], |acc, mappings| {
+                acc.iter().flat_map(|r| mappings.map_range(r)).collect()
+            })
     }
 }
 
@@ -67,21 +136,33 @@ impl Solver for Problem {
         let lines = file_reader.lines().map(|x| x.unwrap()).collect_vec();
 
         let (_, seed_list) = lines[0].split(": ").collect_tuple().unwrap();
-        let seeds = seed_list.split(" ").map(|x| x.parse().unwrap()).collect();
+        let seeds = seed_list
+            .split(" ")
+            .map(|x| x.parse().unwrap())
+            .collect_vec();
+        let seed_ranges = seeds.chunks(2).map(|v| v[0]..(v[0] + v[1])).collect_vec();
 
         let mut mappings = Vec::new();
         let mut current_mapping = Vec::new();
         for i in 3..lines.len() {
             if lines[i].contains("map") {
-                mappings.push(current_mapping);
+                mappings.push(Mappings {
+                    mappings: current_mapping,
+                });
                 current_mapping = Vec::new();
             } else if lines[i] != "" {
                 current_mapping.push(lines[i].parse().unwrap());
             }
         }
-        mappings.push(current_mapping);
+        mappings.push(Mappings {
+            mappings: current_mapping,
+        });
 
-        Almanac { seeds, mappings }
+        Almanac {
+            seeds,
+            mappings,
+            seed_ranges,
+        }
     }
 
     fn solve_first(&self, input: &Self::Input) -> Result<Self::Output1, String> {
@@ -94,6 +175,37 @@ impl Solver for Problem {
     }
 
     fn solve_second(&self, input: &Self::Input) -> Result<Self::Output2, String> {
-        todo!()
+        Ok(input
+            .seed_ranges
+            .iter()
+            .map(|seed_range| input.get_seed_range_location(seed_range))
+            .map(|ranges| ranges.iter().map(|range| range.start).min().unwrap())
+            .min()
+            .unwrap())
     }
+}
+
+fn merge_ranges(ranges: &Vec<Range<usize>>) -> Vec<Range<usize>> {
+    let mut result = Vec::new();
+
+    let sorted = ranges
+        .iter()
+        .sorted_by(|a, b| a.start.cmp(&b.start))
+        .collect_vec();
+
+    for range in sorted {
+        if result.len() == 0 {
+            result.push(range.clone());
+            continue;
+        }
+
+        let i = result.len() - 1;
+        if result[i].end >= range.start {
+            result[i].end = result[i].end.max(range.end);
+        } else {
+            result.push(range.clone());
+        }
+    }
+
+    result
 }
